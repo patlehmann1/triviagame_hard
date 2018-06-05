48522cf7751244ab40b52c43013109e876a708bd
Patrick Lehmann <patlehmann1@gmail.com>
2018-06-05 18:50:43 -0400

    updated JSON

---
+++ assets/javascript/app.js
@@ -0,0 +1,55 @@
+var game = {
+    question1: {
+        q: "When was the first world cup played?",
+        a1: "904",
+        a2: "1975",
+        a3: "1890",
+        a4: "1930",
+        answer: "1930",
+    },
+    question2: {
+        q: "Who was the top goal scorer at the 1986 World Cup?",
+        a1: "Gary Lineker",
+        a2: "Rudi Voller",
+        a3: "Socrates",
+        a4: "Diego Maradona",
+        answer: "Gary Lineker",
+    },
+    question3: {
+        q: "This team won the 2014 World Cup in Brazil:",
+        a1: "France",
+        a2: "Spain",
+        a3: "Germany",
+        a4: "Italy",
+        answer: "Germany",
+    },
+    question4: {
+        q: "Which Colombian player scored an own goal in a group stage match against the USA at the 1994 World Cup and was murdered upon returning to his home country?",
+        a1: "Adolfo Valencia",
+        a2: "John Harold Lozano",
+        a3: "Hernan Gaviria",
+        a4: "Andres Escobar",
+    },
+    question5: {
+        q: "Italy qualified for the World Cup in 2018",
+        a1: "True",
+        a2: "False",
+    },
+    question6: {
+        q: "How many World Cups have been won by the host country?",
+        a1: "2",
+        a2: "6",
+        a3: "2",
+        a4: "9",
+    },
+    question7: {
+        q: "Only one country has been in every World Cup Since 1930. Who is it?",
+        a1: "Brazil",
+        a2: "Germany",
+        a3: "England",
+        a4: "Argentina",
+    },
+}
+
+console.log (game.question7.q);
+console.log(game.question7.a2);
